1eff93af9914106791c834e2de2f1f58db4d0198
vatsaldiv <divyamshreevatsal1998@gmail.com>
2019-09-17 17:17:35 +0530

    update

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ import threading
 import os
 
 loopTimeInSeconds = 10
-last_count = 0
-last_light = 0.0
-last_temp = 0.0
+
+
 
 
 #View Class
@@ -18,22 +17,42 @@ class mywindow(QtWidgets.QMainWindow):
         super(mywindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.getData(self,last_temp,last_light)
+        #self.ui.getData(self,last_temp,last_light)
 
     def updateView(self):
         self.ui.retranslateUi(self)
 
 #Function Definations
 def UpdateDB_Thread():
-    count = getLastCount()
+    #count = getLastCount()
+    last_count = 1
+    last_light = 20.3
+    last_temp = 25.2
+
+    today = date.today()
+    now = datetime.now()
+
+    current_date = today.strftime("%d/%m/%Y")
+    current_time = now.strftime("%H:%M:%S")
+    current_datetime = current_date + "_" + current_time
     while True:
         #Get your data here
 
         #Save your data here
-        updateDB(count,last_temp,54,34)
+        updateDB(last_count,last_temp,last_light,current_datetime)
        
         #Updation
-        count += 1
+        last_count += 1
+        last_light += 1
+        last_temp += 1
+
+        today = date.today()
+        now = datetime.now()
+
+        current_date = today.strftime("%d/%m/%Y")
+        current_time = now.strftime("%H:%M:%S")
+        current_datetime = current_date + "_" + current_time
+
         sleep(loopTimeInSeconds)
 
        
@@ -50,3 +69,21 @@ thread1.start()
 
 thread2 = threading.Thread(target = OpenView_Thread,args=())
 thread2.start()
+
+
+'''
+ser = serial.Serial(port='COM15', baudrate=115200)
+
+bytes_data = ser.read(size = 3)
+print(bytes_data)
+
+encoding = 'utf-8'
+sting_data = str(bytes_data, encoding)
+print(string_data)
+
+values = string_data.strip().split(',')
+last_count, last_light, last_temp = [str(s) for s in values]
+
+ser.write('a')
+'''
+
